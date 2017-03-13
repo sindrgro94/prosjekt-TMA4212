10b7ba6bed1e497d75e27327d8758b140fc9ae48
sindrgro94 <sindre.grostad@icloud.com>
2017-03-13 13:26:43 +0100

    fullDiscretization ser da ut som en bølge hvertfall

--- fullDiscretization.m
+++ fullDiscretization.m
@@ -1,5 +1,5 @@
 function fullDiscretization(M,N)
-    % Solving AX = b
+    % Solving AX = B
     x0 = -5;
     xend = 5;
     t0 = 0;
@@ -13,8 +13,12 @@ function fullDiscretization(M,N)
     t = linspace(t0,tend,N+1);
     b = (xend-x0)/(M+1);
     k = (tend-t0)/N;
+    B = zeros(2*M,1);
     uPrev = zeros(M,1);
     hPrev = makeH0(M,x);
+    if length(hPrev) == 1
+        return;
+    end
     e = ones(M,1);
     X = zeros(2*M,N+1);
     X(1:M,1) = uPrev;
@@ -22,12 +26,28 @@ function fullDiscretization(M,N)
     for time = 2:N+1
         uLeft = spdiags([k*hPrev, 0*e, -k*hPrev],-1:1,M,M)';
         uRight = spdiags([k*uPrev,2*b*e,-k*uPrev],-1:1,M,M)';
-
-
+        lLeft = spdiags([k*hPrev.*uPrev,2*b*hPrev, -k*hPrev.*uPrev],-1:1,M,M)';
+        lRight = spdiags([k*g*hPrev,0*e, -k*g*hPrev],-1:1,M,M)';
+        A = [uLeft, uRight; lLeft, lRight];
+        B(1) = 2*b*hPrev(1)+k*hPrev(1)*u0+k*uPrev(1)*h0;
+        B(2:M-1) = 2*b*hPrev(2:M-1);
+        B(M) = 2*b*hPrev(M)-k*hPrev(M)*uend-k*uPrev(M)*hend;
+        B(M+1) = 2*b*hPrev(1)*uPrev(1)+k*hPrev(1)*uPrev(1)*u0+k*g*hPrev(1)*h0;
+        B(M+2:2*M-1) = 2*b*hPrev(2:M-1).*uPrev(2:M-1);
+        B(2*M) = 2*b*hPrev(M)*uPrev(M)-k*hPrev(M)*uPrev(M)*uend-k*g*hPrev(M)*hend;
+        X(:,time) = A\B;
+        uPrev = X(1:M,time);
+        hPrev = X(M+1:2*M,time);
     end
+    for i = 1:ceil(N/400):N
+        plot(x(2:M+1),X(M+1:2*M,i));
+        ylim([0,2]); 
+        xlim([-5,5]);
+        F(i) = getframe;
+     end
 end
 function h = makeH0(M,x)
-    h0 = @(x) 1+1/5*exp(-1/2*(x).^2/0.1);
+    h0 = @(x) 1+1/2*exp(-1/2*(x).^2/0.1);
     if M>=20
        h = ones(M,1); 
        hstart = round(M/2-M/5);
