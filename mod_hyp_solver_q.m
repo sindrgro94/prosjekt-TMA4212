e29295f753aa67ac5ea5d989aa3c7dcf433340b9
Halvorsg <halvor.snersrud@gmail.com>
2017-03-19 19:27:21 +0100

    flere

--- mod_hyp_solver_q.m
+++ mod_hyp_solver_q.m
@@ -1,9 +1,9 @@
 function H = mod_hyp_solver_q(M,N,time)
 %mod_hyp_solver_q(500,10000,90)
-h0 = @(x) 1+1/3*exp(-1/2*(x).^2/0.1);
+h0 = @(x) 1+1/3*exp(-1/2*(x-2).^2/0.1);
 %% Initierer x,t,H og Q
-xf = -20;
-xt = 20;
+xf = -4;
+xt = 4;
 L = xt-xf;
 x = linspace(xf,xt,M);
 h = x(2)-x(1);
@@ -13,13 +13,20 @@ H = zeros(N,M);
 H(1,:) = h0(x);
 Q = zeros(N,M);
 disp(k/h);
+B = zeros(1,length(x));
+for i = 1:length(x)
+    if x(i) >= -0.5 && x(i) <= 0.5
+        B(i) = 1/3*(cos(x(i)*2*pi)+1);
+    end
+end
+H(1,:) = H(1,:)-B;
 % B = @(x) x*0;
-B = @(x) 0.1*(sin(pi*(3*(x-xf)/L))+x/L);
-Bd = @(x) 0.1*(pi/L * cos(pi*(x-xf)/L) + 1/L);
+% B = @(x) 1*(sin(pi*(3*(x-xf)/L)));
+% Bd = @(x) 0.1*(pi/L * cos(pi*(x-xf)/L) + 1/L);
 %% The actual method
 % H = lax_Friedrich_cons(H,Q,M,N,k,h);
-H = lax_Friedrich_seaBed(H,B,x,Q,M,N,k,h);
-% H = richtmeyer_Ground_BC(H,Q,M,N,k,h);
+% H = lax_Friedrich_grunn(H,B,x,Q,M,N,k,h);
+H = richtmeyer_BC_grunn(H,B,x,Q,M,N,k,h);
 % H = richtmeyer(H,Q,M,N,k,h);
 % H = richtmeyer_BC(H,Q,M,N,k,h);
 % H = lax_wendroff(H,Q,M,N,k,h); %Denne blir fort ustabil
@@ -30,12 +37,12 @@ H = lax_Friedrich_seaBed(H,B,x,Q,M,N,k,h);
 % H = fullDiscretization(M,N,time)';
 %% Movie
 % plotWave(H,x,t,B)
-close all
+% close all
      for i = 1:N %
-        plot(x,H(i,:)+B(x)); %plot wave
+        plot(x,H(i,:)+B); %plot wave
         hold on
-        plot(x,B(x));% plot sea bed
-        ylim([0+min(min(B(x))),2]); 
+        plot(x,B);% plot sea bed
+        ylim([min(min(0)),1.4]); 
         xlim([xf,xt]); % guarantee consistent height
         F(i) = getframe;  % capture it
         pause(time/(15*N)-0.03);
