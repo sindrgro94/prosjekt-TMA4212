0e7037c81aac82d33ff1f939c6c1b19d207c6a20
Halvorsg <halvor.snersrud@gmail.com>
2017-03-19 19:26:42 +0100

    Variabel grunn

--- errorplot.m
+++ errorplot.m
@@ -3,45 +3,45 @@ function errorplot()
 %Finner loglog-plot for feilen i mod_hyp_solver_q.m
 time = 0.5;
 %% finner feil i tiden
-fig = figure;
-subplot(1,2,1);
-tic
-Href_t = mod_hyp_solver_q(100,2^15,time);
-toc
-cnt = 1;
-for i = 7:0.5:9
-    tic
-    N = floor(2^i);
-    h(cnt) = 1/(N+2);
-    H = mod_hyp_solver_q(100,N,time);
-    err = H(end,:)-Href_t(end,:);
-    e(cnt) = norm(err)*sqrt(h(cnt));
-    cnt = cnt+1;
-    toc
-end
-loglog(h,e,'o-r')
-hold on
-loglog(h,h.^2*100,'--')
-loglog(h,h,'--')
-legend('Error in 2-norm','O(k^2)','O(k)')
-title('Convergence plot wrt t')
-xlabel('Steplength')
-ylabel('Error')
-set(gca,'fontsize',18)
-hold off
-ordent = polyfit(log(h),log(e),1);
+% fig = figure;
+% subplot(1,2,1);
+% tic
+% Href_t = mod_hyp_solver_q(100,2^15,time);
+% toc
+% cnt = 1;
+% for i = 7:0.5:9
+%     tic
+%     N = floor(2^i);
+%     h(cnt) = 1/(N+2);
+%     H = mod_hyp_solver_q(100,N,time);
+%     err = H(end,:)-Href_t(end,:);
+%     e(cnt) = norm(err)*sqrt(h(cnt));
+%     cnt = cnt+1;
+%     toc
+% end
+% loglog(h,e,'o-r')
+% hold on
+% loglog(h,h.^2*100,'--')
+% loglog(h,h,'--')
+% legend('Error in 2-norm','O(k^2)','O(k)')
+% title('Convergence plot wrt t')
+% xlabel('Steplength')
+% ylabel('Error')
+% set(gca,'fontsize',18)
+% hold off
+% ordent = polyfit(log(h),log(e),1);
 %% Finner feil i rommet
 tic
-steg = 2^15;
-Href = mod_hyp_solver_q(steg,1000,time);
+steg = 2^10;
+Href = mod_hyp_solver_q(steg,15000,time);
 toc
 cnt = 1;
-subplot(1,2,2);
+% subplot(1,2,2);
 for i = 6:9
     tic
     M = round(2^i);
     h1(cnt) = 40/(M+2);
-    H = mod_hyp_solver_q(M,1000,time);
+    H = mod_hyp_solver_q(M,15000,time);
     x = round(steg/2^i);
     tel = 1;
     for j = 1:x:steg
@@ -64,7 +64,7 @@ set(gca,'fontsize',18)
 hold off
 xlim([min(h1),max(h1)])
 ordenx = polyfit(log(h1),log(e1),1);
-fprintf('F?r tidsteg av orden %1.2f \n', ordent(1));
+% fprintf('F?r tidsteg av orden %1.2f \n', ordent(1));
 fprintf('F?r romsteg av orden %1.2f \n', ordenx(1));
 %saveTightFigure(fig,'Figurer/errorFullDiscretization.pdf');
 end
