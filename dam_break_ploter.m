3f715c12c6590e0fbbcf91fffa4a2fee2f81f659
sindrgro94 <sindre.grostad@icloud.com>
2017-03-26 11:40:55 +0200

    dam break

--- dam_break_ploter.m
+++ dam_break_ploter.m
@@ -1,17 +1,39 @@
-function dam_break_ploter()
-[Ht,Bt,~] = dam_break_solver(true);
-[Hf,Bf,x] = dam_break_solver(false);
+%% dam_break_ploter
+clear all
+close all
+clc
+method = 'lax friedrich';
+[Ht,Bt,~] = dam_break_solver(true,method);
+[Hf,Bf,x] = dam_break_solver(false,method);
 [N,~] = size(Hf);
 figure
-    for i = 1:round(N/300):N
-        plot(x,Ht(i,:)+Bt); %Plot wave
-        hold on
-        plot(x,Hf(i,:)+Bf); %Plot wave
-        plot(x,Bf);
-        plot(x,Bt); %sea bed
-        ylim([0,2])
-        xlim([0,x(end)]); % Consistent width
-        F = getframe;
-        hold off
+for i = 1:round(N/300):N
+    plot(x,Ht(i,:)+Bt,'r'); %Plot wave
+    hold on
+    plot(x,Hf(i,:)+Bf,'b'); %Plot wave
+    plot(x,Bt,'r'); %sea bed
+    plot(x,Bf,'b');
+    ylim([0,2])
+    xlim([0,x(end)]); % Consistent width
+    hold off
+end
+%% make subplot
+fig = figure;
+time = [1 round(N/7) round(N/4)];
+for i = 1:3
+    subplot(1,3,i)
+    plot(x,Ht(time(i),:)+Bt,'r'); %Plot wave
+    hold on
+    plot(x,Hf(time(i),:)+Bf,'b'); %Plot wave
+    plot(x,Bt,'r'); %sea bed
+    plot(x,Bf,'b'); 
+    ylim([0,2])
+    xlim([0,x(end)]); % Consistent width
+    legend('uneven sea-bed','flat sea-bed')
+    set(gca,'fontsize',18)
+    hold off
+    if i == 2
+        title('Dam break')
     end
 end
+% saveTightFigure(fig,'Figurer/figureName.pdf');
--- dam_break_solver.m
+++ dam_break_solver.m
@@ -1,7 +1,8 @@
-function [H,B,x] = dam_break_solver(groundFunction)
+function [H,B,x] = dam_break_solver(groundFunction,method)
 M1 = 1000;
 tEnd = 1;
-x0=0; xEnd = 1;
+x0=0; 
+xEnd = 1;
 x = linspace(x0,xEnd,M1);
 countdown = true;
 
@@ -12,59 +13,61 @@ switch (groundFunction)
     otherwise
         B = zeros(1,length(x)); % Flat sea bed
 end
-
-
-%% First Lax-Friedrich
-N = 500;
-M1 = 1000;
-x = linspace(x0,xEnd,M1);
-h = x(2)-x(1);
-t = linspace(0,0.01,5000);
-k = t(2)-t(1);
-H1 = zeros(N,M1);
-Q1 = zeros(N,M1);
-H1(1,:) = dam_break2(x)-B;
-[H1,Q1] = lax_Friedrich_grunn(H1,B,x,Q1,M1,N,k,h,countdown);
-%% Second Richtmyer
-N = 10000;
-M2 = 1000;
-H = zeros(N,M2);
-Q = zeros(N,M2);
-if M2 ~= M1
-    step = floor(M1/M2); cnt = 1;
-    for i = 1:step:length(H1(end,:))
-        H(1,cnt) = H1(end,i);
-        Q(1,cnt) = Q1(end,i);
-        B(cnt) = B(i);
-        cnt = cnt+1;
+if strcmp(method, 'richtmeyer')
+    %% First Lax-Friedrich
+    N = 500;
+    M1 = 1000;
+    x = linspace(x0,xEnd,M1);
+    h = x(2)-x(1);
+    t = linspace(0,0.01,5000);
+    k = t(2)-t(1);
+    H1 = zeros(N,M1);
+    Q1 = zeros(N,M1);
+    H1(1,:) = dam_break2(x)-B;
+    [H1,Q1] = lax_Friedrich_grunn(H1,B,x,Q1,M1,N,k,h,countdown);
+    %% Second Richtmyer
+    N = 10000;
+    M2 = 1000;
+    H = zeros(N,M2);
+    Q = zeros(N,M2);
+    if M2 ~= M1
+        step = floor(M1/M2); cnt = 1;
+        for i = 1:step:length(H1(end,:))
+            H(1,cnt) = H1(end,i);
+            Q(1,cnt) = Q1(end,i);
+            B(cnt) = B(i);
+            cnt = cnt+1;
+        end
+    else
+        H(1,:) = H1(end,:);
+        Q(1,:) = Q1(end,:);
     end
-else
-    H(1,:) = H1(end,:);
-    Q(1,:) = Q1(end,:);
+    x = linspace(x0,xEnd,M2);
+    h = x(2)-x(1);
+    t = linspace(0.001,tEnd,5000);
+    k = t(2)-t(1);
+    H = richtmeyer_BC_grunn(H,B,x,Q,M2,N,k,h,countdown);
+else 
+    N = 5000;
+    M1 = 1000;
+    x = linspace(x0,xEnd,M1);
+    h = x(2)-x(1);
+    t = linspace(0,1,N);
+    k = t(2)-t(1);
+    H1 = zeros(N,M1);
+    Q1 = zeros(N,M1);
+    H1(1,:) = dam_break2(x)-B;
+    [H,Q] = lax_Friedrich_grunn(H1,B,x,Q1,M1,N,k,h,countdown);
+
 end
-x = linspace(x0,xEnd,M2);
-h = x(2)-x(1);
-t = linspace(0.001,tEnd,5000);
-k = t(2)-t(1);
-H = richtmeyer_BC_grunn(H,B,x,Q,M2,N,k,h,countdown);
-%% Plotting
-fig = figure;
-    for i = 1:round(N/300):N
-        plot(x,H(i,:)+B); %Plot wave
-        hold on
-        ylim([0,2])
-        xlim([x0,xEnd]); % Consistent width
-        plot(x,B); % Plot sea bed
-        hold off
-        F = getframe;
-    end
 end
 
 function B = bottom(x,widthOfBump)
+    peakPos = 1;
     B = zeros(1,length(x));
     for i = 1:length(x)
-        if x(i) >= -widthOfBump+0.5 && x(i) <= widthOfBump+0.5
-            B(i) = 1/9*(cos((x(i)-0.5)*2*pi/(2*widthOfBump))+1);
+        if x(i) >= -widthOfBump+peakPos && x(i) <= widthOfBump+peakPos
+            B(i) = 1/5*(cos((x(i)-peakPos)*2*pi/(2*widthOfBump))+1);
         end
     end
 end
